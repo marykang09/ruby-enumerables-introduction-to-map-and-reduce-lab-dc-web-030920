9341a95c99f4b60b2386895c30552364f1974571
Mary Kang <mary.kang09@gmail.com>
2020-03-01 21:50:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,7 +41,7 @@ new_array
 end
 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
 i = 0
 total = starting_point
 while i < source_array.length do
